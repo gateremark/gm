747c05e2e0f22070cc74da27863b1262b9c42791
gateremark <mark5gatere@gmail.com>
2023-12-22 14:33:52 +0300

    fix: toast only one notification

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,46 @@ import LinkButton from "./components/LinkButton";
 import { SlSocialLinkedin, SlSocialTwitter } from "react-icons/sl";
 // import Rainmp4 from "./assets/rain.mp4";
 // import Rainwebm from "./assets/rain.webm";
+import { Toaster, toast } from "sonner";
+import { useState } from "react";
 import {
-	TbNotebook,
-	TbWorld,
-	TbUser,
-	TbBrandGithub,
-	TbStack2,
-	TbNotes,
+    TbNotebook,
+    TbWorld,
+    TbUser,
+    TbBrandGithub,
+    TbStack2,
+    TbNotes,
 } from "react-icons/tb";
 
 interface HeaderLink {
-	name: string;
-	link: string;
-	icon: JSX.Element;
+    name: string;
+    link: string;
+    icon: JSX.Element;
 }
 
 interface LinkData {
-	name: string;
-	link: string;
-	icon: JSX.Element;
-	className?: string;
+    name: string;
+    link: string;
+    icon: JSX.Element;
+    className?: string;
 }
 
 function App() {
-	const headerLinks: HeaderLink[] = [
-		{
-			name: "LinkedIn",
-			link: "https://www.linkedin.com/in/gateremark/",
-			icon: <SlSocialLinkedin />,
-		},
-		{
-			name: "Twitter",
-			link: "https://twitter.com/gatere_mark",
-			icon: <SlSocialTwitter />,
-		},
-	];
+    const [showToast, setShowToast] = useState(false);
+    const headerLinks: HeaderLink[] = [
+        {
+            name: "LinkedIn",
+            link: "https://www.linkedin.com/in/gateremark/",
+            icon: <SlSocialLinkedin />,
+        },
+        {
+            name: "Twitter",
+            link: "https://twitter.com/gatere_mark",
+            icon: <SlSocialTwitter />,
+        },
+    ];
 
-	const linksData: LinkData[] = [
+    const linksData: LinkData[] = [
         {
             name: "GitHub",
             link: "https://github.com/gateremark",
@@ -62,64 +65,76 @@ function App() {
         },
         {
             name: "My Portfolio",
-            link: "https://gateremark.vercel.app/",
+            link: "https://www.gatere.tech/",
             icon: <TbWorld className=" text-[35px]" />,
         },
         {
             name: "My Résumé",
-            link: "https://drive.google.com/file/d/1IDsb6o6bjtoWNoZFgsy5F4oGZB3dB8o-/view?usp=drive_link",
+            link: "https://drive.google.com/file/d/1s9P87dURW58p_9WDzczvnYqQ3y20OuT7/view?usp=drive_link",
             icon: <TbUser className=" text-[35px]" />,
         },
     ];
-	return (
-		<div className="relative">
-			<video
-				autoPlay
-				muted
-				loop
-				id="myVideo"
-				className="absolute inset-0 w-full h-full object-cover opacity-50"
-			>
-				<source
-					src="https://res.cloudinary.com/dvuazircp/video/upload/v1699399509/rain_opicg6.webm"
-					type="video/webm"
-				/>
-			</video>
-			<div className="relative z-10 flex flex-col justify-center items-center mx-3 md:mx-12">
-				<img
-					className="mt-12 h-24 rounded-full"
-					src={Gatere}
-					alt="gateremark"
-				/>
-				<p className=" text-[#fff] my-3 text-lg font-bold">@gateremark</p>
-				<p className=" text-[#fff] text-base text-center">
-					| MERN Stack | Machine Learning | #foreverlearner
-				</p>
-				<div className="flex text-[#fff] text-3xl gap-5 my-7 justify-center items-center">
-					{headerLinks.map((data, index) => (
-						<a
-							key={index}
-							className="hover:scale-110 transition duration-150 hover:text-[#2867b2] cursor-pointc"
-							href={data.link}
-							target="_blank"
-							rel="noopener"
-						>
-							{" "}
-							{data.icon}{" "}
-						</a>
-					))}
-				</div>
-				<div className="text-[#BDBDBD] flex flex-col gap-5 md:w-[55%] w-full items-center justify-center">
-					{linksData.map((data, index) => (
-						<LinkButton {...data} key={index} />
-					))}
-				</div>
-				<p className="text-[#fff] my-10 text-center">
-					Made with ❤️ by gateremark
-				</p>
-			</div>
-		</div>
-	);
+
+    if (showToast) {
+        toast.success("Link copied to clipboard");
+        setShowToast(false);
+    }
+    return (
+        <div className="relative">
+            <Toaster richColors position="top-right" closeButton />
+            <video
+                autoPlay
+                muted
+                loop
+                id="myVideo"
+                className="absolute inset-0 w-full h-full object-cover opacity-50"
+            >
+                <source
+                    src="https://res.cloudinary.com/dvuazircp/video/upload/v1699399509/rain_opicg6.webm"
+                    type="video/webm"
+                />
+            </video>
+            <div className="relative z-10 flex flex-col justify-center items-center mx-3 md:mx-12">
+                <img
+                    className="mt-12 h-24 rounded-full"
+                    src={Gatere}
+                    alt="gateremark"
+                />
+                <p className=" text-[#fff] my-3 text-lg font-bold">
+                    @gateremark
+                </p>
+                <p className=" text-[#fff] text-base text-center">
+                    | MERN Stack | Machine Learning | #foreverlearner
+                </p>
+                <div className="flex text-[#fff] text-3xl gap-5 my-7 justify-center items-center">
+                    {headerLinks.map((data, index) => (
+                        <a
+                            key={index}
+                            className="hover:scale-110 transition duration-150 hover:text-[#2867b2] cursor-pointc"
+                            href={data.link}
+                            target="_blank"
+                            rel="noopener"
+                        >
+                            {" "}
+                            {data.icon}{" "}
+                        </a>
+                    ))}
+                </div>
+                <div className="text-[#BDBDBD] flex flex-col gap-5 md:w-[55%] w-full items-center justify-center">
+                    {linksData.map((data, index) => (
+                        <LinkButton
+                            {...data}
+                            key={index}
+                            setShowToast={setShowToast}
+                        />
+                    ))}
+                </div>
+                <p className="text-[#fff] my-10 text-center">
+                    Made with ❤️ by gateremark
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,16 +1,16 @@
 import { TbShare2 } from "react-icons/tb";
-import { Toaster, toast } from "sonner";
 
 interface LinkButtonProps {
     name: string;
     link: string;
     icon: JSX.Element;
+    setShowToast?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const LinkButton: React.FC<LinkButtonProps> = ({ name, link, icon }) => {
+const LinkButton: React.FC<LinkButtonProps> = ({ name, link, icon, setShowToast }) => {
     return (
         <div className=" relative w-full text-center bg-[#00000060] hover:bg-[#00000070] backdrop-blur-[3px] shadow-3xl rounded-[30px] hover:scale-[102%] transition duration-300 cursor-pointc">
-            <Toaster richColors position="top-right" closeButton />
+            
             <a href={link} target="_blank" rel="noopener">
                 <div className="flex justify-between items-center ml-2 mr-4">
                     {icon}
@@ -21,9 +21,10 @@ const LinkButton: React.FC<LinkButtonProps> = ({ name, link, icon }) => {
             <TbShare2
                 className="text-[40px] hover:bg-[#00000070] transition ease-in-out duration-300 rounded-full p-2 absolute z-10 right-2 top-3 cursor-pointer"
                 title="Share Link"
-                onClick={(e: any) => {
-                    e.preventDefault();
-                    toast.success("Link Copied Successfully!");
+                onClick={() => {
+                    if (setShowToast) {
+                        setShowToast(true);
+                    }
                     navigator.clipboard.writeText(link);
                 }}
             />
